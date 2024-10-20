97a67b1d211dcefbe26d4cd0fe338973cbdbc750
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-20 01:25:42 -0300

    Criando variaveis para armazenar cores e estilos globais.

--- FrontEnd/DSCarrosTop/src/App.tsx
+++ FrontEnd/DSCarrosTop/src/App.tsx
@@ -1,14 +1,17 @@
-import Header from './components/Header'
-import SectionCars from './components/SectionCars'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SectionCars from "./components/SectionCars";
+import SectionComments from "./components/SectionComments";
 
 function App() {
-
   return (
-<>
-<Header />
-<SectionCars />
-</>
-  )
+    <>
+      <Header />
+      <SectionCars />
+      <SectionComments />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
