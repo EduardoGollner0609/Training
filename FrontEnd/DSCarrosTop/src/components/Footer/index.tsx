a4dd5f558df104fe0562e9c68c423504d4cb3d94
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-20 01:24:26 -0300

    Criando componente Footer.

---
+++ FrontEnd/DSCarrosTop/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <footer>
+        <div className="footer-contact">
+        <address>@dscarrostop</address>
+      <address>
+        <i>Rua da Pipoca, 150</i>
+      </address>
+        </div>
+  
+    </footer>
+  );
+}
