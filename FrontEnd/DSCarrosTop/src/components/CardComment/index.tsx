1579f7d62a15d9bda7eac52640e453feea78e2b9
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-20 01:24:03 -0300

    Criando componente CardComment.

---
+++ FrontEnd/DSCarrosTop/src/components/CardComment/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function CardComment() {
+  return (
+    <div className="card-comment">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+        voluptatem ad fugit id distinctio.
+      </p>
+    </div>
+  );
+}
