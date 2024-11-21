d8fec05f86afe7f35e12c8859971ec1fd7b48f29
EduGollner0609 <dudugollner@gmail.com>
2024-11-21 10:08:16 -0300

    Criando método para salvar.

--- BackEnd/TDDEventCity/src/main/java/com/devsuperior/demo/services/CityService.java
+++ BackEnd/TDDEventCity/src/main/java/com/devsuperior/demo/services/CityService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
 
 @Service
@@ -17,6 +18,12 @@ public class CityService {
 
 	public List<CityDTO> findAll() {
 		return repository.findAll(Sort.by(Sort.Direction.ASC, "name")).stream().map(city -> new CityDTO(city)).toList();
+	}
+
+	public CityDTO insert(CityDTO cityDTO) {
+		City city = new City();
+		city.setName(cityDTO.getName());
+		return new CityDTO(repository.save(city));
 	}
 
 }
