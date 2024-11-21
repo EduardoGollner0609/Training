0215c6590dfae5e1bef562151e52c3469fb1cdc7
EduGollner0609 <dudugollner@gmail.com>
2024-11-21 10:58:43 -0300

    Relacionamento User e Role.

--- BackEnd/TDDEventCity/src/main/java/com/devsuperior/demo/entities/User.java
+++ BackEnd/TDDEventCity/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,9 +1,15 @@
 package com.devsuperior.demo.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +21,10 @@ public class User {
 	private Long id;
 	private String email;
 	private String password;
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 	public User() {
 	}
@@ -47,6 +57,23 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
