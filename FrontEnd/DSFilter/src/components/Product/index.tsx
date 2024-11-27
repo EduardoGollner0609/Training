27c874f8aec379bebb952a542f47245e55b973c2
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 12:50:22 -0300

    Adicionando estilos/css para Product.

--- FrontEnd/DSFilter/src/components/Product/index.tsx
+++ FrontEnd/DSFilter/src/components/Product/index.tsx
@@ -1,6 +1,8 @@
+import './styles.css';
+
 export default function Product() {
     return (
-        <div>
+        <div className="product">
             <p>PC Gamer</p>
             <h3>R$ 1200.00</h3>
         </div>
