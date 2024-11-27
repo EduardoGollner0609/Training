4e72fedea1c1344a66a6a228837b2d6baa52f7ee
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 14:12:22 -0300

    Instanciando contexto de estado global dos produtos.

--- FrontEnd/DSFilter/src/App.tsx
+++ FrontEnd/DSFilter/src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css'
 import Header from './components/Header'
 import ListingBody from './components/ListingBody'
+import * as productService from './services/product-service';
 
 function App() {
 
+  const [contextProductCount, setContextProductCount] = useState<number>(productService.findAllSortedPrice.length);
 
   return (
     <>
