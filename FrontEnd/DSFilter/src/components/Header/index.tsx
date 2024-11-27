93f0906d9357e2551cef2a5191269b6dbe147a54
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 12:19:03 -0300

    Criando componente Header.

---
+++ FrontEnd/DSFilter/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>DSFilter</h1>
+                <p>3 produto(s)</p>
+            </nav>
+        </header>
+    );
+}
