47dfdd6d51d0893fa8a7232c474fd119b76aaa46
EduGollner0609 <dudugollner@gmail.com>
2024-11-27 12:36:05 -0300

    Adicionando estilos ao componente Filter.

--- FrontEnd/DSFilter/src/components/Filter/index.tsx
+++ FrontEnd/DSFilter/src/components/Filter/index.tsx
@@ -1,8 +1,11 @@
+import './styles.css';
+
+
 export default function Filter() {
     return (
-        <div>
-            <input type="text" />
-            <input type="text" />
+        <div className="filter">
+            <input type="text" placeholder='Preço mínimo' />
+            <input type="text" placeholder='Preço máximo' />
             <button>Filtrar</button>
         </div>);
 }
