1c41fbaac9fc66b1bdff739df0a1458a6edf4e4b
EduGollner0609 <dudugollner@gmail.com>
2024-11-28 09:07:27 -0300

    retornando movie pelo id.

---
+++ BackEnd/MovieFlix/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+
+	public MovieDetailsDTO findById(Long id) {
+		Movie movie = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Não encontrado"));
+		return new MovieDetailsDTO(movie);
+	}
+}
